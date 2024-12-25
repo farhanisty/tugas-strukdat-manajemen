2a7ee42f1dac068c573ff5ca6b0c2a9920e19d0e
farhanisty <farhannivtasija@gmail.com>
2024-12-25 12:26:39 +0700

    create Product Entity

---
+++ src/entity/Product.hpp
@@ -0,0 +1,16 @@
+#ifndef Entity_Product_HPP
+#define Entity_Product_HPP
+
+#include <string>
+
+using std::string;
+
+namespace Entity {
+struct Product {
+  string code;
+  string name;
+  float price;
+};
+} // namespace Entity
+
+#endif
