d655d08fdd3c08f7641ea74cfe6080fbdd70e780
farhanisty <farhannivtasija@gmail.com>
2024-12-26 01:55:38 +0700

    implement ModernMenuRenderer in main page

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include "core/page/LoopPage.hpp"
+#include "custom/renderer/ModernMenuRenderer.hpp"
 #include "pages/MainPage.hpp"
 #include <iostream>
+#include <locale>
 
 int main() {
+  std::wcout.imbue(std::locale("en_US.UTF-8"));
+
   Pages::MainPage *mainPage = new Pages::MainPage();
+  auto basicRenderer = mainPage->getRenderer();
+
+  Custom::Renderer::ModernMenuRenderer *modernMenuRenderer =
+      new Custom::Renderer::ModernMenuRenderer("MENU MANAJEMEN TOKO");
+
+  delete basicRenderer;
+
+  mainPage->changeRenderer(modernMenuRenderer);
+
   Core::Page::LoopPage *loopPage = new Core::Page::LoopPage(mainPage);
 
   loopPage->execute();
