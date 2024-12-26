135c0b6f02788dcb14ddfbb798fcabc23e7a67ae
farhanisty <farhannivtasija@gmail.com>
2024-12-26 23:27:27 +0700

    create product file handler

---
+++ src/file/ProductFileHandler.hpp
@@ -0,0 +1,28 @@
+#ifndef File_ProductFileHandler_HPP
+#define File_ProductFileHandler_HPP
+
+#include "../entity/Product.hpp"
+#include <string>
+#include <vector>
+
+using Entity::Product;
+
+namespace File {
+class ProductFileHandler {
+  std::string filename;
+  void overwriteFile(const std::vector<Product *> products);
+
+public:
+  ProductFileHandler(std::string filename);
+
+  bool create(const Product *product);
+
+  std::vector<Product *> get();
+
+  // void update(const std::string code, Product *updatedProduct);
+  //
+  // void remove(const std::string code);
+};
+} // namespace File
+
+#endif
