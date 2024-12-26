cfa823de9262da806d4ca22416e44cb9dbdf745f
farhanisty <farhannivtasija@gmail.com>
2024-12-27 00:43:52 +0700

    perform main page as simple page

--- src/pages/MainPage.cpp
+++ src/pages/MainPage.cpp
@@ -7,14 +7,8 @@
 using Pages::MainPage;
 
 void MainPage::configureMenu() {
-  Pages::Show::ShowPage *showPage = new Show::ShowPage();
-  auto tempRenderer = showPage->getRenderer();
-
-  showPage->changeRenderer(
-      new Custom::Renderer::ModernMenuRenderer("OPSI LANJUT"));
-
   this->addMenu("Input Produk", new Pages::Insert::InsertPage());
-  this->addMenu("Tampil Produk", showPage);
+  this->addMenu("Tampil Produk", new Show::ShowPage());
   this->addMenu("Hapus Produk", new Pages::Show::ShowPage());
   this->addMenu("History", new Pages::Show::ShowPage());
   this->addMenu("exit", new Core::Page::ExitPage(this));
